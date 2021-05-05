5decc0784bf5e43ee5358de6b7d0c866afa39631
Icaka <ico.ezekiev@gmail.com>
2021-05-05 20:32:07 +0300

    Drawing the field
    it's a 2d array of rectangles

--- Snake/Game.cpp
+++ Snake/Game.cpp
@@ -2,10 +2,21 @@
 
 const unsigned WINDOW_WIDTH = 640;
 const unsigned WINDOW_HEIGHT = 480;
-
+const unsigned RECT_SIZE = 25;
 Game::Game() : window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "My snakw")
 {
-	
+	for (int i = 0; i < MATRIX_SIZE; i++)
+	{
+		for (int t = 0; t < MATRIX_SIZE; t++)
+		{
+			matrix[i][t].setSize(sf::Vector2f(RECT_SIZE, RECT_SIZE));
+			
+			matrix[i][t].setFillColor(sf::Color(128, 235, 138));
+			matrix[i][t].setOutlineThickness(1);
+			matrix[i][t].setOutlineColor(sf::Color::Black);
+			matrix[i][t].setPosition(sf::Vector2f(i * RECT_SIZE, t * RECT_SIZE));
+		}
+	}
 }
 
 void Game::run()
@@ -25,12 +36,20 @@ void Game::processEvents()
 	{
 		if (event.type == sf::Event::KeyPressed)
 		{
-			//handlePlayerInput(event.key.code);
+			handlePlayerInput(event.key.code);
 		}
 		if (event.type == sf::Event::Closed)
 		{
 			window.close();
 		}
+	}
+}
+
+void Game::handlePlayerInput(sf::Keyboard::Key key)
+{
+	if (key == sf::Keyboard::X)
+	{
+		window.close();
 	}
 }
 
@@ -42,6 +61,8 @@ void Game::update()
 void Game::render()
 {
 	window.clear();
-
+	for (int i = 0; i < MATRIX_SIZE; i++)
+		for (int t = 0; t < MATRIX_SIZE; t++)
+			window.draw(matrix[i][t]);
 	window.display();
 }
--- Snake/Game.h
+++ Snake/Game.h
@@ -1,9 +1,14 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+
+const int MATRIX_SIZE = 10;
+
 class Game
 {
 private:
 	sf::RenderWindow window;
+	sf::RectangleShape matrix[MATRIX_SIZE][MATRIX_SIZE];
+
 	void processEvents();
 	void update();
 	void render();
