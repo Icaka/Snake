cc50271bd018ac0208f21a1eee855ec444d8144f
Icaka <ico.ezekiev@gmail.com>
2021-05-07 19:20:34 +0300

    Apple that can be eaten
    tail still doesn't grow

--- Snake/Game.cpp
+++ Snake/Game.cpp
@@ -1,10 +1,12 @@
 #include "Game.h"
-
+#include <iostream>
 const unsigned WINDOW_WIDTH = 640;
 const unsigned WINDOW_HEIGHT = 480;
 const unsigned RECT_SIZE = 25;
 Game::Game() : window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "My snakw")
 {
+	directionX = 0;
+	directionY = 0;
 	for (int i = 0; i < MATRIX_SIZE; i++)
 	{
 		for (int t = 0; t < MATRIX_SIZE; t++)
@@ -19,8 +21,14 @@ Game::Game() : window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "My snakw")
 	}
 
 	head.setCoordinates(3, 3);
-	tail[0].setCoordinates(1, 3);
-	tail[1].setCoordinates(2, 3);
+	tail[0].setCoordinates(1, 5);
+	tail[1].setCoordinates(1, 4);
+	tail[2].setCoordinates(1, 4);
+	tail[3].setCoordinates(1, 3);
+	tail[4].setCoordinates(2, 3);
+
+	placeApple();
+	score = 0;
 }
 
 void Game::run()
@@ -54,6 +62,7 @@ void Game::handlePlayerInput(sf::Keyboard::Key key)
 	if (key == sf::Keyboard::X)
 	{
 		window.close();
+		std::cout << "Score: " << score << std::endl;
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
 	{
@@ -81,22 +90,58 @@ void Game::update()
 {
 	if (directionX != 0 || directionY != 0) // check if it's moving
 	{
-		matrix[tail[0].getX()][tail[0].getY()].setFillColor(sf::Color(128, 235, 138));
-		for (int i = 0; i < tailLength - 1; i++)
-		{
-			tail[i].x = tail[i + 1].x;
-			tail[i].y = tail[i + 1].y;
-		}
-		tail[tailLength - 1].x = head.x;
-		tail[tailLength - 1].y = head.y;
+		moveSnake();
 	}
-	head.x += directionX;
-	head.y += directionY;
+	if (head.x == applePosX && head.y == applePosY)
+	{
+		matrix[applePosX][applePosY].setFillColor(sf::Color(128, 235, 138));
+		score++;
+		placeApple();
+	}
 	//snake.drawToMatrix(matrix);
 	for(int i = 0; i < tailLength; i++)
 		matrix[tail[i].getX()][tail[i].getY()].setFillColor(sf::Color(100, 51, 153));
 	matrix[head.getX()][head.getY()].setFillColor(sf::Color(153, 51, 153));
+	matrix[applePosX][applePosY].setFillColor(sf::Color(255, 112, 77));
 	sf::sleep(sf::milliseconds(100));
+}
+
+void Game::moveSnake()
+{
+	matrix[tail[0].getX()][tail[0].getY()].setFillColor(sf::Color(128, 235, 138));
+	for (int i = 0; i < tailLength - 1; i++)
+	{
+		tail[i].x = tail[i + 1].x;
+		tail[i].y = tail[i + 1].y;
+	}
+	tail[tailLength - 1].x = head.x;
+	tail[tailLength - 1].y = head.y;
+
+	head.x += directionX;
+	head.y += directionY;
+	if (head.x < 0)
+		head.x = MATRIX_SIZE - 1;
+	if (head.x >= MATRIX_SIZE)
+		head.x = 0;
+	if (head.y < 0)
+		head.y = MATRIX_SIZE - 1;
+	if (head.y >= MATRIX_SIZE)
+		head.y = 0;
+}
+
+void Game::placeApple()
+{
+	applePosX = rand() % MATRIX_SIZE;
+	applePosY = rand() % MATRIX_SIZE;
+	//std::cout << "applepos: " << applePosX << ". " << applePosY << std::endl;
+	if (head.getX() == applePosX && head.getY() == applePosY)
+		placeApple();
+	for (int i = 0; i < tailLength; i++)
+	{
+		if (tail[i].getX() == applePosX && tail[i].getY() == applePosY)
+			placeApple();
+	}
+	
 }
 
 void Game::render()
--- Snake/Game.h
+++ Snake/Game.h
@@ -2,24 +2,29 @@
 #include <SFML/Graphics.hpp>
 #include "SnakePart.h"
 const int MATRIX_SIZE = 15;
-
+const int STARTING_TAIL = 5;
 class Game
 {
 private:
 	sf::RenderWindow window;
 	sf::RectangleShape matrix[MATRIX_SIZE][MATRIX_SIZE];
 	SnakePart head;
-	int tailLength = 2;
-	SnakePart tail[2];
+	int tailLength = STARTING_TAIL;
+	SnakePart tail[STARTING_TAIL];
 	//Snake snake;
-	int directionX = 0;
-	int directionY = 0;
+	int directionX;
+	int directionY;
+	int applePosX;
+	int applePosY;
+
+	int score;
 protected:
 	void processEvents();
 	void update();
 	void render();
 	void handlePlayerInput(sf::Keyboard::Key);
 	void moveSnake();
+	void placeApple();
 public:
 	Game();
 	void run();
